5ae10f52d09ac45468b9633b8d30d1aacbd40634
Muhammad Assagaf <muhammadassagaf46@gmail.com>
2020-10-12 08:34:19 +0700

    hello world

---
+++ hello/carrots.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+
+using namespace std;
+
+
+int main() {
+	int n;
+	int p;
+	string uwu;
+	scanf("%i %i", &n, &p);
+	for (int i=0; i < n; i++) {cin >> uwu;}
+	cout << p;
+    return 0;
+}
---
+++ hello/r2.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+
+using namespace std;
+
+
+int main() {
+	int r1;
+	int s;
+	scanf("%i %i", &r1, &s);
+	cout << (s * 2 - r1);
+    return 0;
+}
